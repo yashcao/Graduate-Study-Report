bfef1a0048061ab0b362d682238dac86cb3eff54
e <yashcao@163.com>
2018-05-21 22:01:47 +0800

    update and add

--- 2018年搬的砖/SD-based 信道估计论文复现/CoSaMP.m
+++ 2018年搬的砖/SD-based 信道估计论文复现/CoSaMP.m
@@ -7,7 +7,6 @@ m = size(Phi,2); %
 n = size(z,2); % m x n 的稀疏矩阵
 a = zeros(m, n); % 初始的稀疏矩阵 256x1
 
-
 v = z - Phi * a;
 it=0;
 stop = 0;
@@ -16,28 +15,39 @@ while ~stop
     y = Phi'*v;
     % y = y./repmat( sqrt(sum(abs(Phi).^2,1)'/M),1,n); % 归一化
     % [temp1, temp2] = sort(sum(abs(y).^2,2),'descend');
-    [~, temp] = sort(sum(abs(y).^2,2),'descend');
+    % [~, temp] = sort(sum(abs(y).^2,2),'descend');
+    [~, temp] = sort(abs(y),'descend');
     Omega =temp(1:2*remaind_max_iter);
     
     if it==0
         T = Omega;
     else
-        T = union(Omega, T_last);
+        T = union(Omega, T_last); % 从小到大排序
     end
     % T_last = T;
     %Signal Estimation
     b = zeros(m, n);
     T_index = T;
-    b(T_index,:) = Phi(:, T_index) \ z; % 类似于LS，得到保留位置抽头
-    %Prune
     
+    %Prune
+    Phi2 = Phi(:,T_index);
+    b(T_index,:) = inv(Phi2'*Phi2)*Phi2'*z; % LS
+    %b(T_index,:) = Phi(:, T_index) \ z; % 类似于LS，得到保留位置抽头
+    %b(T_index,:) = pinv(Phi2)*z; % LS
+
     % a = b; % 类似于LS，得到保留位置抽头，其他置为0
     
     % 在估计值中选绝对值最大的 k 列
     [~,temp2] = sort(abs(b),'descend');
-    T_index =temp2(1:remaind_max_iter);
-    a(T_index,:) = b(T_index,:); % 类似于LS，得到保留位置抽头，其他置为0
-    T_last = T_index;
+    %[~, temp2] = sort(sum(abs(b).^2,2),'descend');
+    T_index2 =temp2(1:remaind_max_iter);
+
+    
+    % 保留问题
+    a = zeros(m,n);
+    a(T_index2,:) = b(T_index2,:); % 类似于LS，保留位置抽头，其他置为0
+    T_last = T_index2;
+    
     %Sample Update
     v = z - Phi*a;
     %Iteration counter
@@ -48,7 +58,7 @@ while ~stop
       stop = 1;
     end
 end
-
+ 
 % 发射天线
-support = T_last;
+support = unique(T_last);
 used_iter = it;
---
+++ 2018年搬的砖/SD-based 信道估计论文复现/README.md
@@ -0,0 +1,27 @@
+# 论文 Reliable Beamspace Channel Estimation for Millimeter-Wave Massive MIMO Systems with Lens Antenna Array
+
+官方代码不完整，和论文有出入，根据论文修改代码。
+
+## 发现
+
+- 信道分布的系数严重影响了仿真结果。
+- 论文公式正负号的问题。
+- **CoSaMP算法的效果理论上应该比OMP算法好，但是实验仿真结果并非如此。问题留待以后解决**。
+
+----------------------------
+## reference
+
+This simulation code package is mainly used to reproduce the results of the following paper:
+
+Linglong Dai, Xinyu Gao, Shuangfeng Han, Chih-Lin I, and Xiaodong Wang, "Beamspace channel estimation for millimeter-wave massive MIMO systems with lens antenna array," in Proc. IEEE ICCC'16, Chengdu, China, Jul. 2016
+
+The author in charge of this simulation code pacakge is: Xinyu Gao (email: xy-gao14@mails.tsinghua.edu.cn).
+
+Reference: We highly respect reproducible research, so we try to provide the simulation codes 
+for our published papers (more information can be found at: http://oa.ee.tsinghua.edu.cn/dailinglong/). 
+If you in any way use this code for research that results in publications, please cite our original paper 
+listed above.
+
+Copyright reserved by the Broadband Communications and Signal Processing Laboratory
+(led by Dr. Linglong Dai), Department of Electronic Engineering, Tsinghua University, 
+Beijing 100084, China. 
